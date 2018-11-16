e8bc1509d9f5aa7f81cba3572593c9837981306f
seanmcquaid <sean.mcquaid2@verizon.net>
2018-11-16 10:14:25 -0500

    Algorithm #1 done

--- python algorithms #1/algorithm1.py
+++ python algorithms #1/algorithm1.py
@@ -1,7 +1,8 @@
 user_number = int(raw_input("What's your maximum number? "))
 
+sum = 0
 for i in range (0, user_number):
-    if (i % 3 == 0 or i % 5 == 0):
-        print i
-    else:
-        pass
+    if ((i % 3 == 0) or (i % 5 == 0)):
+        sum += i
+
+print sum
