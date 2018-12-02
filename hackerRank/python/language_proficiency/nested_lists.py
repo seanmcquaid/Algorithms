b5559baa669b658a5bdfb7be5979d207e0c9303c
seanmcquaid <sean.mcquaid2@verizon.net>
2018-12-02 17:46:07 -0500

    solved nested lists problem with some help

--- hackerRank/python/language_proficiency/nested_lists.py
+++ hackerRank/python/language_proficiency/nested_lists.py
@@ -3,14 +3,18 @@
 
 # Note: If there are multiple students with the same grade, order their names 
 # alphabetically and print each name on a new line.
+N = int(raw_input())
 
-if __name__ == '__main__':
-    for _ in range(int(input())):
-        name = input()
-        score = float(input())
-        class_list = []
-        student_profile =[]
-        student_profile.append(name)
-        student_profile.append(score)
-        class_list.append(student_profile)
-        print (class_list[0])
+students = list()
+for i in range(N):
+    students.append([raw_input(), float(raw_input())])
+
+scores = set([students[x][1] for x in range(N)])
+scores = list(scores)
+scores.sort()
+
+second_students = [x[0] for x in students if x[1] == scores[1]]
+second_students.sort()
+
+for s in second_students:
+    print s
