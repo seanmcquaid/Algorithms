46ec29e3c097a74ea99c6a36c4aff3feeca4037d
seanmcquaid <sean.mcquaid2@verizon.net>
2018-12-09 19:46:44 -0500

    solved 2nd problem

--- javascript algorithms/JS exercises/objectExercises.js
+++ javascript algorithms/JS exercises/objectExercises.js
@@ -22,5 +22,23 @@ function commonYear(){
 commonYear(array);
 
 // Given a string “This is a random string”, write a function that returns a count of each character in the string
-// Given an array of olympic race results [{country: “usa”, time: 233}, {country: “poland”, time: 222}, … ], write a function that returns each country’s best time
+var randomString = "This is a random string";
+
+function letterFrequency(){
+    var object = {};
+    for(i = 0; i < randomString.length; i++){
+        var currentItem = randomString[i];
+        if(!object[currentItem]){
+            object[currentItem] = 0;
+        }
+        object[currentItem] += 1;
+    }
+    console.log(object);
+}
+
+letterFrequency(randomString);
+// Given an array of olympic race results [{country: “usa”, time: 233}, {country: “poland”, time: 222}, … ],
+//  write a function that returns each country’s best time
+
+
 // Given a list of numbers, [-1, 5, -25, -3, 99, 20], write a function that returns the count of positive numbers and negative numbers
