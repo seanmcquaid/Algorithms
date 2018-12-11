5099ce92981b75dcfe8bb8d7f262efd6a1f7dbd0
seanmcquaid <sean.mcquaid2@verizon.net>
2018-12-10 23:17:51 -0500

    started saturday algorithm but couldn't find solution

--- javascript algorithms/algorithms/saturdays.js
+++ javascript algorithms/algorithms/saturdays.js
@@ -1,2 +1,28 @@
 // January 1, 1701 was a Saturday. 
 // How many Saturdays were on the first of the month between 1701 and 1800?
+var startDate = new Date("January, 1, 1701");
+var endDate = new Date("December, 31, 1800");
+var startYear = startDate.getFullYear();
+var endYear = endDate.getFullYear();
+var startMonthIndex = startDate.getMonth();
+var endMonthIndex = endDate.getMonth();
+var firstDayOfMonth = startDate.getDate();
+var lastDayOfMonth = endDate.getDate();
+var dayOfWeek = startDate.getDay();
+
+function saturdays(){
+    var saturdayCount = 0;
+    for(i = startYear; i < endYear; i++){
+        for(i = startMonthIndex; i < endMonthIndex; i++){
+            for(i = firstDayOfMonth; i < lastDayOfMonth; i++){
+                if(firstDayOfMonth == 1){
+                    if(dayOfWeek == 6){
+                        saturdayCount += 1;
+                    }
+                }
+            }
+        } console.log(saturdayCount);
+    }
+}
+
+saturdays();
