e745a1e78b879640b23164a0f69e5fba2d730874
seanmcquaid <sean.mcquaid2@verizon.net>
2019-01-13 23:10:24 -0500

    insertion solution fixed

--- javascript algorithms/algorithms/insertionSort.js
+++ javascript algorithms/algorithms/insertionSort.js
@@ -1,12 +1,12 @@
 let numList = [5,2,6,1,4,3];
 let sorted = false;
-for (let i = 0; i < unsorted.length; i++){
-    let first = unsorted[i]
+for (let i = 1; i < numList.length; i++){
+    let first = numList[i]
     let j = i - 1
-    while (j >= 0 && unsorted[j] > first){
-        unsorted[j + 1] = unsorted[j]
+    while (j >= 0 && numList[j] > first){
+        numList[j + 1] = numList[j]
         j--
     }
-    unsorted[j + 1] = first
-    console.log(unsorted)
+    numList[j + 1] = first
 }
+console.log(numList)
