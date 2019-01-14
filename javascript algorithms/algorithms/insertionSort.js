5187d9a8a699ff3809c9188c51804a3a2cf975ea
seanmcquaid <sean.mcquaid2@verizon.net>
2019-01-13 21:10:15 -0500

    using a similar approach to my bubblesort

--- javascript algorithms/algorithms/insertionSort.js
+++ javascript algorithms/algorithms/insertionSort.js
@@ -2,14 +2,18 @@ let array = [5,2,6,1,4,3];
 let sorted = false;
 
 while(sorted === false){
-    sorted = true;
+    let count = 0;
     for(i = 0; i < array.length; i++){
         if(array[i] > array[i+1]){
             let stash = array[i];
             array[i] = array[i+1];
             array[i+1] = stash;
             sorted = false;
+            count++;
         }
+    }
+    if(count == 0){
+        sorted = true;
     }
 }
 console.log(array);
