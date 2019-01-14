d6fc8847468af858e3a04e1714262f8152e84611
seanmcquaid <sean.mcquaid2@verizon.net>
2019-01-13 21:29:22 -0500

    fixed bubblesort so it looks cleaner

--- javascript algorithms/algorithms/bubbleSort.js
+++ javascript algorithms/algorithms/bubbleSort.js
@@ -1,22 +1,18 @@
-let numList = [5,4,11,2,6];
-let sorted = false;
-while (sorted === false){
+let numList = [5,2,6,1,4,3];
+while(sorted == false){
     let count = 0;
-    for(let i = 0; i < numList.length-1; i++){
-        let currentNum = i;
-        let nextNum= i+1;
-        if(numList[currentNum] > numList[nextNum]){
-            let stashNumber = numList[currentNum];
-            numList[currentNum] = numList[nextNum];
-            numList[nextNum] = stashNumber;
-            count++;
-        }
+    for(let i = 0; i < numList.length; i++){
+      let firstCompNumber = numList[i];
+      let secondCompNumber = numList[i+1];
+      if (secondCompNumber < firstCompNumber){
+          secondCompNumber = firstCompNumber;
+          firstCompNumber = secondCompNumber;
+          count++;
+      }
     }
-    if(count == 0){
+    if (count == 0){
         sorted = true;
     }
 }
-
-
 
 console.log(numList);
