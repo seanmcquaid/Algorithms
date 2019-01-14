6ede1940c30680f0d48571b86cd7b117093c9d1d
seanmcquaid <sean.mcquaid2@verizon.net>
2019-01-13 23:16:18 -0500

    clearly labeled numbers

--- javascript algorithms/algorithms/insertionSort.js
+++ javascript algorithms/algorithms/insertionSort.js
@@ -1,12 +1,11 @@
 let numList = [5,2,6,1,4,3];
-let sorted = false;
 for (let i = 1; i < numList.length; i++){
-    let first = numList[i]
-    let j = i - 1
-    while (j >= 0 && numList[j] > first){
-        numList[j + 1] = numList[j]
-        j--
+    let firstNum = numList[i]
+    let previousNumIndex = i - 1
+    while (previousNumIndex >= 0 && numList[previousNumIndex] > firstNum){
+        numList[previousNumIndex + 1] = numList[previousNumIndex];
+        secondNumIndex--;
     }
-    numList[j + 1] = first
+    numList[secondNumIndex + 1] = firstNum;
 }
 console.log(numList)
