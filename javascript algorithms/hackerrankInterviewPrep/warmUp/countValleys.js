2ba7e0b4b595bdf10bf6859b73bc4720071f0ef4
seanmcquaid <sean.mcquaid2@verizon.net>
2019-01-15 10:14:51 -0500

    making progress on counting valleys

--- javascript algorithms/hackerrankInterviewPrep/warmUp/countValleys.js
+++ javascript algorithms/hackerrankInterviewPrep/warmUp/countValleys.js
@@ -1,4 +1,23 @@
+let numberOfSteps = 10;
+let path = "DDUDDUUDUU";
+
 function countingValleys(num, string) {
-
-
+    let valleyCount = 0; 
+    let multipleDCount = 0;
+    let stringArray =string.split("")
+    for(let i = 0; i < stringArray.length; i++){
+        if(stringArray[i] === "D"){
+            multipleDCount++;
+        } else if (stringArray[i] === "U"){
+            if(multipleDCount > 1){
+                multipleDCount = 0;
+                valleyCount++;
+            } else if (multipleDCount = 1){
+                multipleDCount = 0;
+            }
+        }
+    }
+    console.log(valleyCount);
 }
+
+countingValleys(numberOfSteps, path);
