944dd8952946fa8bc442481724c70af62c204a86
seanmcquaid <sean.mcquaid2@verizon.net>
2019-01-18 11:16:08 -0500

    fixed group selector

--- javascript algorithms/algorithms/groupPicker.js
+++ javascript algorithms/algorithms/groupPicker.js
@@ -1,11 +1,16 @@
 const students = ['Brandon','Khanh','Connor','Cody','Christopher','Sean','Katie','Jason','Michael','Gbinga','Greg','Bryan','Matt','Zac','Rob', 'Ron'];
-let newStudents = [];
+let newStudents = [
+    [],
+    [],
+    [],
+    []
+ ];
 
 while(students.length > 0){
     for(let i = 0; i < 4; i++){
         let randIndex = Math.floor(Math.random() * students.length);
         const student = students.splice(randIndex, 1);
-        newStudents.push(student[0]);
+        newStudents[i].push(student[0]);
     }
 }
 
