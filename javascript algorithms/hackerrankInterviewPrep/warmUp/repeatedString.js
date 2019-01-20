3811b3a455d27218c496c31eeeb2c6b22b44de55
seanmcquaid <sean.mcquaid2@verizon.net>
2019-01-19 22:54:42 -0500

    starting repeated string algorithm

---
+++ javascript algorithms/hackerrankInterviewPrep/warmUp/repeatedString.js
@@ -0,0 +1,10 @@
+let numberOfChar = 6;
+let string ="hello";
+
+function repeatedString(s, n) {
+    let stringSplit = s.split("");
+    let charArray = [];
+    
+}
+
+repeatedString(string, numberOfChar);
