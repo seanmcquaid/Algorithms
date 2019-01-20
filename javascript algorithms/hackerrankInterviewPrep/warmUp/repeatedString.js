b7b0c132b2b7c06b829741c9ea6219954a5801ab
seanmcquaid <sean.mcquaid2@verizon.net>
2019-01-19 23:22:25 -0500

    created inefficient solution

--- javascript algorithms/hackerrankInterviewPrep/warmUp/repeatedString.js
+++ javascript algorithms/hackerrankInterviewPrep/warmUp/repeatedString.js
@@ -1,10 +1,26 @@
-let numberOfChar = 6;
-let string ="hello";
+let numberOfChar = 1000000000000;
+let string ="a";
 
 function repeatedString(s, n) {
-    let stringSplit = s.split("");
+    let stringSplit = string.split("");
     let charArray = [];
-    
+    let count = 0;
+
+    while(charArray.length < numberOfChar){
+        for(let i = 0; i < stringSplit.length; i++){
+            charArray.push(stringSplit[i]);
+            if(charArray.length === numberOfChar){
+                break
+            }
+        }
+    }
+
+    for(let i = 0; i < charArray.length; i++){
+        if(charArray[i] == "a"){
+            count++;
+        }
+    }
+    return count;
 }
 
 repeatedString(string, numberOfChar);
