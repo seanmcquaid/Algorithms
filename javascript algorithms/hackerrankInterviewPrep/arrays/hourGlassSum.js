f0473b85c076e86fda0859a4685aa04f71b0494f
seanmcquaid <sean.mcquaid2@verizon.net>
2019-02-12 17:01:39 -0500

    made algorithm more expandable

--- javascript algorithms/hackerrankInterviewPrep/arrays/hourGlassSum.js
+++ javascript algorithms/hackerrankInterviewPrep/arrays/hourGlassSum.js
@@ -34,15 +34,17 @@ let array = [
 
 function hourglassSum(arr) {
     let biggestHourGlass = -82;
-    for(let i = 0; i < 4; i++){
-        for(let j = 0; j < 4; j++){
+    let gridCheck = arr.length - 2;
+    console.log(gridCheck)
+    for(let i = 0; i < gridCheck; i++){
+        for(let j = 0; j < gridCheck; j++){
             let hourGlass = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
             if(hourGlass > biggestHourGlass){
                 biggestHourGlass = hourGlass;
             }
         }
     }
-    return(biggestHourGlass);
+    console.log(biggestHourGlass);
 }
 
 hourglassSum(array);
