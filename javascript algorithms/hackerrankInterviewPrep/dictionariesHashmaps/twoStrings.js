0279f39a8a387dded42a68a71bad302542826fd8
seanmcquaid <sean.mcquaid2@verizon.net>
2019-02-19 18:06:59 -0500

    came up with a slightly more efficient solution

--- javascript algorithms/hackerrankInterviewPrep/dictionariesHashmaps/twoStrings.js
+++ javascript algorithms/hackerrankInterviewPrep/dictionariesHashmaps/twoStrings.js
@@ -4,12 +4,13 @@ let str2 = "world"
 function twoStrings(s1, s2) {
     let substring = false;
     for(let i = 0; i < s1.length; i++){
-        for(let j = 0; j < s2.length; j++){
-            if(s1[i] == s2[j]){
-                substring = true;
-                return ("YES")
-            }
+        let regexCheck = RegExp(`[${str1[i]}]+`)
+        let match = regexCheck.test(s2)
+        if( match == true){
+            substring = true;
+            return("YES")
         }
+        
     }
     if(substring == false) {
         return("NO")
