47d046078e61946af4697f9fa70c1e22a2dbd4fa
seanmcquaid <sean.mcquaid2@verizon.net>
2019-03-22 13:36:35 -0400

    added in console logs to track how stack was changing

--- javascript algorithms/algorithms/flattenArray.js
+++ javascript algorithms/algorithms/flattenArray.js
@@ -1,13 +1,14 @@
 const originalArray =  [[0, 1], [2, 3], [4, 5, [6, 7, [8, [9, 10]]]]]
 
-// output must be [0,1,2,3,4,5,6,7,8,9,19]
+// output must be [0,1,2,3,4,5,6,7,8,9,10]
 
 function flattenArr(array){
     const stack = [...array]
-    console.log(stack)
     const resultArray = [];
     while(stack.length){
         const firstItem = stack.shift()
+        console.log(firstItem)
+        console.log(stack)
         if(Array.isArray(firstItem)){
             stack.push(...firstItem)
         } else {
