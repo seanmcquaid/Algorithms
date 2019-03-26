880572031bef53cf5c5547430e832e36ce9d0a9f
seanmcquaid <sean.mcquaid2@verizon.net>
2019-03-25 21:34:16 -0400

    learning more about big o

--- colt-steele-algorithms/big-o-notation/intro.js
+++ colt-steele-algorithms/big-o-notation/intro.js
@@ -24,3 +24,25 @@ function addUpTo(n){
 
 // can use performance.now() before and after function to time results
 // speed is not always precise
+
+// count the number of simple operations to check efficiency
+
+// Big O allows us to talk formally about how the runtime of an algorithm
+// grows as the inputs grow
+
+// we only care about the broad trends
+
+// Definition of Big O
+
+// an algorithm is O(f(n)) if the number of simple operations the computer
+// has to do is eventually less than a constant times f(n) as n increases
+
+// linear = f(n) = n
+
+// quadatric = f(n) = n ^ 2
+
+// constant = f(n) = 1;
+
+// could be different but this is the most basic def
+
+
