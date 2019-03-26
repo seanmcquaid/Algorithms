519c4295b25a2b1c8f06c3a2414563056595aba1
seanmcquaid <sean.mcquaid2@verizon.net>
2019-03-25 23:54:12 -0400

    array methods for big analyzed

--- colt-steele-algorithms/performance-arrays-objects/arrays.js
+++ colt-steele-algorithms/performance-arrays-objects/arrays.js
@@ -14,3 +14,14 @@ let values = [true, {}, [], 2, "awesome"];
 
 // inserting and removing at the end of an array is fine, at the beginning is WAY less efficient
 // inserting and removing at beginning - O(n)
+
+// Big O of Array Operations
+// push - O(1)
+// pop - O(1)
+// shift - O(n)
+// unshift - O(n)
+// concat - O(n)
+// slice - O(n)
+// splice - O(n)
+// sort - O(N * log N)
+// forEach, map, filter, reduce, etc - O(N)
