1d313db4b95f00f9ff413e22a4508a23e67c7438
seanmcquaid <sean.mcquaid2@verizon.net>
2019-03-25 23:46:49 -0400

    array notes for big o

---
+++ colt-steele-algorithms/performance-arrays-objects/arrays.js
@@ -0,0 +1,16 @@
+let names = ["Michael", "Melissa", "Andrea"];
+let values = [true, {}, [], 2, "awesome"];
+
+// ordered lists
+
+// use them when you need order
+// when you need fast access/sertion and removal (sort of )
+
+// Big O of Arrays
+// insertion - it depends
+// removal - it depends
+// searching - O(n)
+// access - O(1)
+
+// inserting and removing at the end of an array is fine, at the beginning is WAY less efficient
+// inserting and removing at beginning - O(n)
