adba110dadd0510b3077952d74e78d161beb4d2c
seanmcquaid <sean.mcquaid2@verizon.net>
2019-03-27 11:03:16 -0400

    came up with solution for freq counter

--- colt-steele-algorithms/problem-solving-patterns/areThereDuplicates.js
+++ colt-steele-algorithms/problem-solving-patterns/areThereDuplicates.js
@@ -5,8 +5,21 @@
 // this can be solved with either the frequency counter pattern or 
 // the multiple pointers pattern
 
+// frequency counter
+
 function areThereDuplicates(){
-    console.log(arguments)
+    // console.log(arguments)
+    let counter = {};
+    for (let i = 0; i < arguments.length; i++){
+        let argument = arguments[i];
+        counter[argument] = (counter[argument] || 0) + 1;
+    }
+    for(let key in counter){
+        if(counter[key] > 1){
+            return true
+        }
+    }
+    return false
 }
 
-areThereDuplicates(1,3,5)
+console.log(areThereDuplicates(1,3,5))
