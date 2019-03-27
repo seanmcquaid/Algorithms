6d8b43d49fd6a72209c014d9b06ffd5cfdf862f4
seanmcquaid <sean.mcquaid2@verizon.net>
2019-03-27 10:59:12 -0400

    learned about using variable arguments and the arguments object in javascript

---
+++ colt-steele-algorithms/problem-solving-patterns/areThereDuplicates.js
@@ -0,0 +1,12 @@
+// implement a function called, areThereDuplicates which accepts
+// a variable number of arguments and checks whether there are any duplicates
+// among the arguments passed in
+
+// this can be solved with either the frequency counter pattern or 
+// the multiple pointers pattern
+
+function areThereDuplicates(){
+    console.log(arguments)
+}
+
+areThereDuplicates(1,3,5)
