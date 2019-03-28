9b2084c5699152ac259cb11326be78f0dc586bba
seanmcquaid <sean.mcquaid2@verizon.net>
2019-03-27 23:35:58 -0400

    started min sub array len but will finish tomorrow

---
+++ colt-steele-algorithms/problem-solving-patterns/minSubArrayLen.js
@@ -0,0 +1,17 @@
+// write a function called minSubArrayLen which accepts two parameters - an array
+// of positive integers and a positive integer
+
+// this function return the minimal length of a continguous subarray
+// of which the sum is greater than or equal to the integer passed to the
+// function. If there isn't one, return 0 instead
+
+const array = [2,3,1,2,4,3];
+const num = 7
+
+function minSubArrayLen(array,num){
+    for(let i = 0; i < array.length; i++){
+        
+    }
+}
+
+minSubArrayLen(array,num)
