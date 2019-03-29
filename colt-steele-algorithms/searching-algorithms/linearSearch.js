b04f88d9cc6aa5df19ddaf9f35ad60d71efb21b2
seanmcquaid <sean.mcquaid2@verizon.net>
2019-03-29 15:58:28 -0400

    notes on linear search

--- colt-steele-algorithms/searching-algorithms/linearSearch.js
+++ colt-steele-algorithms/searching-algorithms/linearSearch.js
@@ -9,3 +9,10 @@ function linearSearch(array,value){
 }
 
 linearSearch([34,56,1,2], 1)
+
+// this is O(N) complexity
+
+// best case : O(1)
+// worst case : O(N)
+
+// this is the best we can do with an unsorted piece of data
