04ebb5643a7ab35701ba3b6df9fbbec6721a4778
seanmcquaid <sean.mcquaid2@verizon.net>
2019-03-30 08:22:49 -0400

    notes on sorting

---
+++ colt-steele-algorithms/bubble-sort/intro.js
@@ -0,0 +1,18 @@
+// elementary sorting algorithms
+
+// what is sorting?
+
+// the process of rearranging the items in a collection (like an array) so that the items appear in some kind of order.
+
+// examples 
+// sorting numbers from smallest to largest
+// sorting names alphabetically
+// sorting movies based on revenue/release year
+
+// why do we need to learn this?
+// sorting is an incredibly common task so it's good to know how it works
+// many different ways to sort things and different techniques have their own adv / disadv
+// classic interview topic!
+
+// built in javascript sorting
+
