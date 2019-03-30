22f11e40a51d621c8494bce6c82fe02cc921ddcf
seanmcquaid <sean.mcquaid2@verizon.net>
2019-03-30 08:36:44 -0400

    learned about comparator functions for sorting

--- colt-steele-algorithms/bubble-sort/intro.js
+++ colt-steele-algorithms/bubble-sort/intro.js
@@ -16,3 +16,20 @@
 
 // built in javascript sorting
 
+// but it doesnt always work the way it should
+// the default sort order is unicode points
+
+// can specify how to sort
+// use a comparator function that looks at a pair and determines their sort order based on the return value
+
+function numberCompare(num1,num2){
+    return num1 - num2
+}
+
+[6,4,5,10].sort(numberCompare)
+
+function compareByLen(str1,str2){
+    return str1.length - str2.length
+}
+
+["Sean", "McQuaid", "Hello"].sort(compareByLen)
