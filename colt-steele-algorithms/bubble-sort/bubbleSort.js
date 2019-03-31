50a838ea280b50218a44c1d9abb2c90293a7dea5
seanmcquaid <sean.mcquaid2@verizon.net>
2019-03-31 17:21:52 -0400

    made a slightly more optimized solution

--- colt-steele-algorithms/bubble-sort/bubbleSort.js
+++ colt-steele-algorithms/bubble-sort/bubbleSort.js
@@ -30,9 +30,27 @@
 
 // upoptimized
 
+// function bubbleSort(arr){
+//     for(let i = 0; i < arr.length; i++){
+//         for(let j = 0; j < arr.length - 1; j++){
+//             if(arr[j] > arr[j+1]){
+//                 // swap
+//                 let temp = arr[j];
+//                 arr[j] = arr[j+1];
+//                 arr[j+1] = temp;
+//             }
+//         }
+//     }
+//     return arr
+// }
+
+// based off pseudo code
+// this way the code runs less comparisons! if you start from the end, the window gets smaller
+
+
 function bubbleSort(arr){
-    for(let i = 0; i < arr.length; i++){
-        for(let j = 0; j < arr.length - 1; j++){
+    for(let i = arr.length; i > 0; i--){
+        for(let j = 0; j < i - 1; j++){
             if(arr[j] > arr[j+1]){
                 // swap
                 let temp = arr[j];
