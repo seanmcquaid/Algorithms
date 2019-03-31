1ccc5cfe15550b640f29a4ebf82e7fff9a239bc8
seanmcquaid <sean.mcquaid2@verizon.net>
2019-03-31 18:01:26 -0400

    fully optimized

--- colt-steele-algorithms/bubble-sort/bubbleSort.js
+++ colt-steele-algorithms/bubble-sort/bubbleSort.js
@@ -65,19 +65,43 @@
 
 // solution using updated swap from ES6
 
-function bubbleSort(arr){
-    const swap = (arr,idx1,idx2) => {
-        [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]
-    }
+// function bubbleSort(arr){
+//     const swap = (arr,idx1,idx2) => {
+//         [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]
+//     }
 
+//     for(let i = arr.length; i > 0; i--){
+//         for(let j = 0; j< arr.length; j++){
+//             if(arr[j] > arr[j+1]){
+//                 swap(arr,j, j+1)
+//             }
+//         }
+//     }
+//     return arr
+// }
+
+// optimized fully
+// this prevents going through an array that's already sorted 
+
+function bubbleSort(arr){
+    let noSwaps;
     for(let i = arr.length; i > 0; i--){
-        for(let j = 0; j< arr.length; j++){
+        noSwaps = true;
+        for(let j = 0; j < i - 1; j++){
             if(arr[j] > arr[j+1]){
-                swap(arr,j, j+1)
+                // swap
+                noSwaps = false;
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
             }
+        }
+        if(noSwaps){
+            break
         }
     }
     return arr
 }
+
 
 console.log(bubbleSort([37,45,29,8]))
