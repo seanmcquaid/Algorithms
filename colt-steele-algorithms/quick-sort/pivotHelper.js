89eee0cd838d56884ede8160740067abe5c441c9
seanmcquaid <sean.mcquaid2@verizon.net>
2019-04-01 19:24:15 -0400

    notes on quick sort

---
+++ colt-steele-algorithms/quick-sort/pivotHelper.js
@@ -0,0 +1,8 @@
+// might be called partition or pivot
+
+// given an array, this helper function should designate an element as the pivot
+// it should then rearrange elements in the array so that the values less than the pivot are moved to the left of the pivot and all values greater than
+// are moved to right of the pivot
+// the order on either side doesn't matter
+// the helper should do this IN PLACe and should not create a new array
+// when complete, the helper should return the index of the pivot
