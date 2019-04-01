97ba68a7305c5105ad10351ae7a42054a6be37c9
seanmcquaid <sean.mcquaid2@verizon.net>
2019-03-31 21:59:32 -0400

    selection sort done

--- colt-steele-algorithms/selection-sort/selectionSort.js
+++ colt-steele-algorithms/selection-sort/selectionSort.js
@@ -1,5 +1,22 @@
 // attempted solution
 
 function selectionSort(array){
-
+    for(let i = 0; i < array.length; i++){
+        let lowest = i;
+        for(let j = i + 1; j < array.length; j++){
+            if(array[j] < array[lowest]){
+                lowest = j;
+            }
+        }
+        if(i !== lowest){
+            let temp = array[i];
+            array[i] = array[lowest];
+            array[lowest] = temp;
+        }
+    }
+    return array
 }
+
+let array = [34,22,10,19,17]
+
+console.log(selectionSort(array));
