a1af16785bb3f44370fb72bc5cd1931f31f55917
seanmcquaid <sean.mcquaid2@verizon.net>
2019-03-31 22:02:56 -0400

    solution with es6 syntax swapping

--- colt-steele-algorithms/selection-sort/selectionSort.js
+++ colt-steele-algorithms/selection-sort/selectionSort.js
@@ -1,20 +1,38 @@
 // attempted solution
 
-function selectionSort(array){
-    for(let i = 0; i < array.length; i++){
+// function selectionSort(array){
+//     for(let i = 0; i < array.length; i++){
+//         let lowest = i;
+//         for(let j = i + 1; j < array.length; j++){
+//             if(array[j] < array[lowest]){
+//                 lowest = j;
+//             }
+//         }
+//         if(i !== lowest){
+//             let temp = array[i];
+//             array[i] = array[lowest];
+//             array[lowest] = temp;
+//         }
+//     }
+//     return array
+// }
+
+function selectionSort(arr){
+    const swap = (arr,index1, index2)=>{
+        ([arr[index1], arr[index2]] = [arr[index2], arr[index1]])
+    }
+
+    for(let i = 0; i < arr.length; i++){
         let lowest = i;
-        for(let j = i + 1; j < array.length; j++){
-            if(array[j] < array[lowest]){
+        for(let j = i + 1; j < arr.length; j++){
+            if(arr[lowest] > arr[j]){
                 lowest = j;
             }
         }
         if(i !== lowest){
-            let temp = array[i];
-            array[i] = array[lowest];
-            array[lowest] = temp;
+            swap(arr,i,lowest)
         }
     }
-    return array
 }
 
 let array = [34,22,10,19,17]
