6a81556795b15387674ea46fcdf5baf70cce8f33
seanmcquaid <sean.mcquaid2@verizon.net>
2019-04-02 13:07:33 -0400

    more efficient solution

--- javascript algorithms/hackerrankInterviewPrep/dictionariesHashmaps/ransomNote.js
+++ javascript algorithms/hackerrankInterviewPrep/dictionariesHashmaps/ransomNote.js
@@ -2,27 +2,21 @@ let magazine = ["ive", "got", "a", "lovely", "bunch", "of", "coconuts"]
 let note = ["Ive", "got", "coconuts"]
 
 function checkMagazine(magazine, note) {
-    let magazineCount = {};
-    for(let i = 0; i < magazine.length; i++){
-        let word = magazine[i];
-        magazineCount[word] = (magazineCount[word] || 0) + 1;
+    let map = {};
+    let recreatable = true;
+    for(let i of magazine){
+        map[i] = (map[i] || 0) + 1;
     }
-    let noteCount = {};
-    for(let i = 0; i < note.length; i++){
-        let word = note[i];
-        noteCount[word] = (noteCount[word] || 0) + 1;
+    for(let i of note){
+        map[i] = (map[i] || 0) - 1;
     }
-    let stringCount = 0;
-    for(let keys in noteCount){
-        if(magazineCount[keys] >= noteCount[keys]){
-            stringCount++;
+    for(let i in map){
+        if(map[i] < 0){
+            recreatable = false;
+            break;
         }
     }
-    if(stringCount === note.length){
-        return "Yes"
-    } else {
-        return "No"
-    }
+    console.log(recreatable? "Yes" : "No")
 }
 
 console.log(checkMagazine(magazine,note))
