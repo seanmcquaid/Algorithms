0673ea77e9430def870a1743c8507f6cbe7b5f74
seanmcquaid <sean.mcquaid2@verizon.net>
2019-04-02 00:16:56 -0400

    helper methods slightly debugged

--- colt-steele-algorithms/radix-sort/helperMethods.js
+++ colt-steele-algorithms/radix-sort/helperMethods.js
@@ -7,9 +7,47 @@
 //     return Math.floor(Math.abs(num)/Math.pow(10,i)) % 10;
 // }
 
+// function getDigit(num, i){
+//     let numString = num.toString().split("")
+//     if(i > numString.length){
+//         return 0
+//     }
+//     let returnIndex = numString.length - i - 1;
+//     let number = parseInt(numString[returnIndex])
+//     return number
+// }
+
+// console.log(getDigit(7423,1))
+
+// // need to find out how many digits are in a number
+// // how many times do we need to sort into buckets
+
+// function digitCount(num){
+//     let numString = num.toString().split("");
+//     return numString.length
+// }
+
+// // console.log(digitCount(2000))
+
+// // need to make a function to see what the most digits we need to check is 
+
+// function mostDigits(arr){
+//     let highestDigitCount = 0;
+//     for(let i = 0; i < arr.length; i++){
+//         let digitAmount = digitCount(arr[i])
+//         if(digitAmount > highestDigitCount){
+//             highestDigitCount = digitAmount
+//         }
+//     }
+//     return highestDigitCount
+// }
+
+console.log(mostDigits([1,3,111,33444]))
+
+
 function getDigit(num, i){
     let numString = num.toString().split("")
-    if(i > numString.length){
+    if(i >= numString.length){
         return 0
     }
     let returnIndex = numString.length - i - 1;
@@ -17,19 +55,10 @@ function getDigit(num, i){
     return number
 }
 
-console.log(getDigit(7423,1))
-
-// need to find out how many digits are in a number
-// how many times do we need to sort into buckets
-
 function digitCount(num){
     let numString = num.toString().split("");
     return numString.length
 }
-
-// console.log(digitCount(2000))
-
-// need to make a function to see what the most digits we need to check is 
 
 function mostDigits(arr){
     let highestDigitCount = 0;
@@ -41,6 +70,3 @@ function mostDigits(arr){
     }
     return highestDigitCount
 }
-
-console.log(mostDigits([1,3,111,33444]))
-
