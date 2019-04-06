425f4caca78b877fbd6ea709900a871382d89092
seanmcquaid <sean.mcquaid2@verizon.net>
2019-04-06 00:11:15 -0400

    turns out, my initial idea was solid but my logic just needed some work

--- javascript algorithms/hackerrankInterviewPrep/stringManipulation/makingAnagrams.js
+++ javascript algorithms/hackerrankInterviewPrep/stringManipulation/makingAnagrams.js
@@ -1,59 +1,43 @@
-let string1 = "cde";
-let string2 = "abc";
+let string1 = "showman";
+let string2 = "womaan";
+
+// string 1 = woman - 2
+// string 2 = woman - 1
 
 function makeAnagram(string1, string2) {
+    let commonCharacters = 0;
     let string1Obj = {};
     let string2Obj = {};
-    let deletionCount = 0;
-    for(let i = 0; i < string1.length; i++){
+    for (let i = 0; i < string1.length; i++) {
         let character = string1[i];
         string1Obj[character] = (string1Obj[character] || 0) + 1;
     }
-    for(let i = 0; i < string2.length; i++){
+    for (let i = 0; i < string2.length; i++) {
         let character = string2[i];
         string2Obj[character] = (string2Obj[character] || 0) + 1;
     }
 
     let longerStringObj, smallerStringObj;
-    if(string1.length > string2.length){
+    if (string1.length >= string2.length) {
         longerStringObj = string1Obj;
-        smallerStringObj = string2Obj;
-    } else if(string1.length === string2.length){
-        longerStringObj = string1Obj;
-        smallerStringObj = string2Obj;
+        smallerStringObj = string2Obj;  
     } else {
         longerStringObj = string2Obj;
         smallerStringObj = string1Obj;
     }
 
     for(let keys in longerStringObj){
-        if(longerStringObj[keys] !== smallerStringObj[keys]){
-            if(smallerStringObj[keys] === undefined){
-                deletionCount += longerStringObj[keys];
-            } else if (longerStringObj[keys] === undefined){
-                deletionCount += smallerStringObj[keys];
-            } else if (longerStringObj[keys] > smallerStringObj[keys]){
-                deletionCount += (longerStringObj[keys] - smallerStringObj[keys])
-            } else if (longerStringObj[keys] < smallerStringObj[keys]){
-                deletionCount += (smallerStringObj[keys] - longerStringObj[keys])
-            }
+        if(longerStringObj[keys] === smallerStringObj[keys]){
+            commonCharacters += longerStringObj[keys] + smallerStringObj[keys];
+        } else if (longerStringObj[keys] > smallerStringObj[keys] && smallerStringObj[keys] !== undefined){
+            commonCharacters += smallerStringObj[keys] * 2
+        } else if (longerStringObj[keys] < smallerStringObj[keys]){
+            commonCharacters += longerStringObj[keys] * 2
         }
     }
-
-    for(let keys in smallerStringObj){
-        if(smallerStringObj[keys] !== longerStringObj[keys]){
-            if(longerStringObj[keys] === undefined){
-                deletionCount += smallerStringObj[keys];
-            } else if (smallerStringObj[keys] === undefined){
-                deletionCount += longerStringObj[keys];
-            } else if (smallerStringObj[keys] > longerStringObj[keys]){
-                deletionCount += (smallerStringObj[keys] - longerStringObj[keys])
-            } else if (smallerStringObj[keys] < longerStringObj[keys]){
-                deletionCount += (longerStringObj[keys] - smallerStringObj[keys])
-            }
-        }
-    }
-    return deletionCount
+    
+    return(string1.length+ string2.length - commonCharacters)
+   
 }
 
 console.log(makeAnagram(string1, string2))
