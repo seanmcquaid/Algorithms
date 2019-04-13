93e5ad0c1832797f3b8a1d41af702c8ed0795651
seanmcquaid <sean.mcquaid2@verizon.net>
2019-04-13 15:08:31 -0400

    reversed a string recursively

--- colt-steele-algorithms/bonus-recursion-problems/reverse.js
+++ colt-steele-algorithms/bonus-recursion-problems/reverse.js
@@ -1,7 +1,20 @@
 // write a function called reverse which accepts a string and returns a new string in reverse
 
 function reverse(string){
-    console.log(string.split("").reverse().join(""))
+    let splitString = string.split("");
+    let reversedString = [];
+    function recursiveReverse(stringArr){
+        if(stringArr.length === 0){
+            return
+        }
+        let lastChar = stringArr.pop()
+        reversedString.push(lastChar);
+        return recursiveReverse(stringArr)
+    }
+    recursiveReverse(splitString)
+
+    console.log(reversedString.join(""))
+
 }
 
 reverse("hi there")
