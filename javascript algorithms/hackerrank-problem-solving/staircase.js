1952b3cd1cf1f2c61ffc0b5337535c8497d8976e
seanmcquaid <sean.mcquaid2@verizon.net>
2019-04-13 00:42:54 -0400

    solved staircase problem

---
+++ javascript algorithms/hackerrank-problem-solving/staircase.js
@@ -0,0 +1,17 @@
+function staircase(n){
+    let count = n;
+    for(let i = 0; i < n; i++){
+        let spacesNum = count - i - 1;
+        let symbolNum = i + 1;
+        let printStair = ""
+        for(let j = 0; j < spacesNum; j++){
+            printStair += " "
+        }
+        for(let k = 0; k < symbolNum; k++){
+            printStair += "#"
+        }
+        console.log(printStair)
+    }
+}
+
+staircase(4)
