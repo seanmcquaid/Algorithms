9155e433c438bfc6129af52eee51553586c96e05
seanmcquaid <sean.mcquaid2@verizon.net>
2019-04-15 17:13:27 -0400

    might have a solution for creating a leaderboard

--- javascript algorithms/hackerrank-problem-solving/climbingLeaderboard.js
+++ javascript algorithms/hackerrank-problem-solving/climbingLeaderboard.js
@@ -1,7 +1,8 @@
 function climbingLeaderboard(scores, alice) {
     let leaderBoardObj = {};
     let leaderBoardArr = [];
-    let aliceRankingArr = []
+    let aliceRankingArr = [];
+    let rankingObj = {}
     for(let i = 0; i < scores.length; i++){
         leaderBoardObj[scores[i]] = (leaderBoardObj[scores[i]] || 0) + 1;
     }
@@ -9,11 +10,14 @@ function climbingLeaderboard(scores, alice) {
     for(let keys in leaderBoardObj){
         leaderBoardArr.push(parseInt(keys))
     }
-    
     leaderBoardArr.reverse();
-    alice.reverse();
-    console.log(leaderBoardArr)
-    console.log(alice)
+    for(let i = 0; i < leaderBoardArr.length; i++){
+        rankingObj[leaderBoardArr[i]] = i + 1
+    }
+    console.log(rankingObj)
+    for(let keys in rankingObj){
+        console.log(parseInt(keys))
+    }
 }
 
 const rankingScores = [100,100,50,40,40,20,10];
