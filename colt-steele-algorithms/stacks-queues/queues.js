5e3e2a3f709ec400624e097f43c2033634638c3d
seanmcquaid <sean.mcquaid2@verizon.net>
2020-05-22 16:11:20 -0400

    finished queues

--- colt-steele-algorithms/stacks-queues/queues.js
+++ colt-steele-algorithms/stacks-queues/queues.js
@@ -15,3 +15,44 @@
 // q.pop();
 // q.pop();
 // q.pop();
+
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Queue {
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    enqueue(val){
+        const newNode = new Node(val);
+        if(!this.first){
+            this.first = newNode;
+            this.last = newNode;
+        }else{
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+        this.size++;
+        return this.size;
+    }
+
+    dequeue(){
+        if(!this.first){
+            return null;
+        }
+        const temp = this.first;
+        if(this.first === this.last){
+            this.last = null;
+        }
+        this.first = this.first.next;
+        this.size--;
+        return temp.value;
+    }
+}
