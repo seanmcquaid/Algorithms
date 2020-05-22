9635e95506b5f3f086d3616ad47cb46680260693
seanmcquaid <sean.mcquaid2@verizon.net>
2020-05-22 15:03:50 -0400

    added example of stack using an array

---
+++ colt-steele-algorithms/stacks-queues/intro.js
@@ -0,0 +1,13 @@
+// STACKS
+
+// A LIFO Data Structure
+
+// The last element added is the first to be removed
+
+// The Call Stack! 
+
+//  Where stacks are used : 
+// Managing function invocation
+// undo / redo functionality
+// Routing the history is treated like a stack (Think React)
+// Algorithms
---
+++ colt-steele-algorithms/stacks-queues/stack.js
@@ -0,0 +1,13 @@
+let stack = [];
+
+stack.push("google");
+stack.push("instagram");
+stack.push("youtube");
+
+stack.pop();
+
+stack.unshift("google");
+stack.unshift("instagram");
+stack.unshift("youtube");
+
+stack.shift();
